b8ea1b1990e481459ffd91042bfb50e407e21c5e
OpenWork-NCE <christiannkengneemmanuel@gmail.com>
2026-06-25 15:25:45 +0200

    feat: add next-intl FR/EN with middleware

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,12 @@
 import { withPayload } from '@payloadcms/next/withPayload'
+import createNextIntlPlugin from 'next-intl/plugin'
+
+const withNextIntl = createNextIntlPlugin('./src/i18n/request.ts')
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    remotePatterns: [{ protocol: 'https', hostname: '**' }],
-  },
+  images: { remotePatterns: [{ protocol: 'https', hostname: '**' }] },
 }
 
-export default withPayload(nextConfig)
+export default withPayload(withNextIntl(nextConfig))
